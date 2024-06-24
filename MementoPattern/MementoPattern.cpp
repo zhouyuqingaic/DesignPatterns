5500739260c1f26d634aa6e27f4091a0b92cf714
zhouyuqing <zhouyuqingaic@outlook.com>
2024-06-24 15:13:07 +0800

    备忘录模式

--- MementoPattern/MementoPattern.cpp
+++ MementoPattern/MementoPattern.cpp
@@ -1,3 +1,65 @@
 //
 // Created by zhouyuqing on 2024/6/18.
 //
+
+#include"MementoPattern.h"
+
+//战士数据备忘录
+int FighterMemento::getLife(){
+    return m_life;
+}
+void FighterMemento::setLife(int life){
+    m_life=life;
+}
+int FighterMemento::getMagic(){
+    return m_magic;
+}
+void FighterMemento::setMagic(int magic){
+    m_magic=magic;
+}
+int FighterMemento::getAttack(){
+    return m_attack;
+}
+void FighterMemento::setAttack(int attack){
+    m_attack=attack;
+}
+
+//玩家主角类
+//创建记录当前玩家数据的备忘录对象
+FighterMemento *Fighter::createFighterMemento() {
+    return new FighterMemento(m_life,m_magic,m_attack);
+}
+//从备忘录中恢复玩家数据
+void Fighter::restoreFighterMemento(FighterMemento *pfm) {
+    m_life=pfm->getLife();
+    m_magic=pfm->getMagic();
+    m_attack=pfm->getAttack();
+}
+//设置玩家死亡
+void Fighter::setToDead() {
+    m_life=0;
+}
+//输出信息
+void Fighter::displayInfo() {
+    cout<<"当前玩家的生命值，魔法值，攻击力分别为:"<<m_life<<","<<m_attack<<","<<m_attack<<endl;
+}
+
+//管理者类
+//析构函数用于释放资源
+MementoManager::~MementoManager() {
+    //释放所有快照
+    for(vector<FighterMemento *>::iterator iter=m_pFighterMementoContainer.begin();iter!=m_pFighterMementoContainer.end();iter++)
+        delete (*iter);
+}
+//获取指向备忘录对象的指针 index指向快照编号
+FighterMemento *MementoManager::getFighterMemento(int index) {
+    vector<FighterMemento *>::iterator iter=m_pFighterMementoContainer.begin();
+    for(int i=0;i<index;i++){
+            ++iter;
+    }
+    return (*iter);
+}
+//设置用于指向备忘录对象的指针
+void MementoManager::setFighterMemento(FighterMemento *p_FighterMemento) {
+    m_pFighterMementoContainer.push_back(p_FighterMemento);
+}
--- MementoPattern/MementoPattern.h
+++ MementoPattern/MementoPattern.h
@@ -5,4 +5,106 @@
 #ifndef DESIGNPATTERNS_MEMENTOPATTERN_H
 #define DESIGNPATTERNS_MEMENTOPATTERN_H
 
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+//前向声明
+class Fighter;
+
+//战士数据备忘录
+class FighterMemento{
+private:
+    friend class Fighter; //声明友元类
+    //构造函数,用private修饰放置在类外被随意创建
+    FighterMemento(int life,int magic,int attack):m_life(life),m_magic(magic),m_attack(attack){}
+    int getLife();
+    void setLife(int life);
+    int getMagic();
+    void setMagic(int magic);
+    int getAttack();
+    void setAttack(int attack);
+private:
+    //战士数据
+    int m_life; //生命值
+    int m_magic; //魔法值
+    int m_attack; //攻击力
+};
+
+//玩家主角类
+class Fighter{
+public:
+    //构造函数
+    Fighter(int life,int magic,int attack):m_life(life),m_magic(magic),m_attack(attack){}
+    //创建记录当前玩家数据的备忘录对象
+    FighterMemento * createFighterMemento();
+    //从备忘录中恢复玩家数据
+    void restoreFighterMemento(FighterMemento * pfm);
+    //设置玩家死亡
+    void setToDead();
+    //输出玩家信息
+    void displayInfo();
+private:
+    //角色属性
+    int m_life; //生命
+    int m_magic; //魔法
+    int m_attack; //攻击
+};
+
+//备忘录管理类
+class MementoManager{
+public:
+    //析构函数用于释放资源
+    ~MementoManager();
+    //获取指向备忘录对象的指针 index为备忘录(快照)编号
+    FighterMemento * getFighterMemento(int index);
+    //设置用于指向备忘录对象的指针
+    void setFighterMemento(FighterMemento * p_FighterMemento);
+private:
+    FighterMemento * m_pFighterMemento; //指向备忘录对象的指针
+    vector<FighterMemento *> m_pFighterMementoContainer; //存储备忘录指针对象的容器
+};
+
+
+
 #endif //DESIGNPATTERNS_MEMENTOPATTERN_H
+
+/*
+
+//使用案例
+
+void test01(){
+    //引入管理者类后
+    Fighter * p_fighter=new Fighter(800,200,300); //创建玩家
+    //显示玩家与Boss战斗前的状态
+    p_fighter->displayInfo();
+
+    //创建备忘录管理器
+    MementoManager * p_memorandumManager=new MementoManager();
+    //创建备忘录对象保存状态的第1个快照
+    p_memorandumManager->setFighterMemento(p_fighter->createFighterMemento());
+
+    //玩家与boss开始战斗
+    cout<<"玩家主角与boss开始激烈的战斗-----"<<endl;
+    p_fighter->setToDead(); //玩家战死
+    p_fighter->displayInfo(); //显示玩家战死后数据
+    //创建备忘录对象保存状态的第2个快照
+    p_memorandumManager->setFighterMemento(p_fighter->createFighterMemento());
+
+    //恢复玩家之前保存的状态
+    cout<<"玩家通过备忘录对象快照0恢复自己的信息------"<<endl;
+    p_fighter->restoreFighterMemento(p_memorandumManager->getFighterMemento(0));
+    p_fighter->displayInfo();
+
+    cout<<"玩家通过备忘录对象快照1恢复自己的信息------"<<endl;
+    p_fighter->restoreFighterMemento(p_memorandumManager->getFighterMemento(1));
+    p_fighter->displayInfo();
+
+
+    //释放资源
+    delete p_memorandumManager;
+    delete p_fighter;
+}
+
+ */
