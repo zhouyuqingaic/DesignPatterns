a038dbf08ebcecd122e45f72c206abc243f4d8cc
zhouyuqing <zhouyuqingaic@outlook.com>
2024-06-24 15:12:50 +0800

    中介者模式

--- MediatorPattern/MediatorPattern.cpp
+++ MediatorPattern/MediatorPattern.cpp
@@ -1,3 +1,144 @@
 //
 // Created by zhouyuqing on 2024/6/18.
 //
+
+#include"MediatorPattern.h"
+
+//ui控件的父类
+//ui控件发生变化后调用
+void CtrlParent::changed(){
+    m_pMediator->ctrlChanged(this); //通知中介者，本ui控件发生变化，所有事情让中介者对象去做
+}
+
+//普通按钮相关类
+//设置按钮的启用或禁用
+void Button::enable(bool sign){
+    if(sign==true)
+        cout<<"按钮<"<<m_caption<<">处于<开启状态>"<<endl;
+    else
+        cout<<"按钮<"<<m_caption<<">处于<关闭状态>"<<endl;
+}
+
+//单选按钮类
+//设置单选按钮的启用或禁用
+void RadioBtn::enable(bool sign){
+    cout<<"单选按钮<"<<m_caption<<">无此功能"<<endl;
+}
+//设置单选按钮被选中或者被取消选中，被选中单选按钮中有个黑色实心圆点
+void RadioBtn::selected(bool sign){
+    if(sign==true)
+        cout<<"单选按钮<"<<m_caption<<">处于<被选中状态>"<<endl;
+    else
+        cout<<"单选按钮<"<<m_caption<<">处于<取消选中状态>"<<endl;
+}
+
+//编辑框类
+//设置编辑框的启用或禁用
+void EditCtrl::enable(bool sign){
+    if(sign==true)
+        cout<<"编辑框<"<<m_caption<<">处于<启用状态>"<<endl;
+    else
+        cout<<"编辑框<"<<m_caption<<">处于<禁用状态>"<<endl;
+}
+//判断编辑框中内容是否为空
+bool EditCtrl::isContentEmpty(){
+    return m_content.empty();
+}
+void EditCtrl::setMContent(string content){
+    m_content=content;
+}
+string EditCtrl::getContent(){
+    return m_content;
+}
+
+//具体中介类
+//析构函数
+ConcertMediator::~ConcertMediator(){
+    //释放资源
+    if(m_pLogin){
+        delete m_pLogin;
+        m_pLogin= nullptr;
+    }
+    if(m_pLogout){
+        delete m_pLogout;
+        m_pLogout= nullptr;
+    }
+    if(m_pRadioBtnAccount){
+        delete m_pRadioBtnAccount;
+        m_pRadioBtnAccount= nullptr;
+    }
+    if(m_pRadioBtnTourist){
+        delete m_pRadioBtnAccount;
+        m_pRadioBtnAccount=nullptr;
+    }
+    if(m_pEditAccount){
+        delete m_pEditAccount;
+        m_pEditAccount=nullptr;
+    }
+    if(m_pEditPassword){
+        delete m_pEditAccount;
+        m_pEditPassword=nullptr;
+    }
+}
+//创建ui控件
+void ConcertMediator::ConcertMediator::createCtrl() {
+    m_pLogin=new Button(this,"登录");
+    m_pLogout=new Button(this,"退出");
+
+    m_pRadioBtnTourist=new RadioBtn(this,"游客登录");
+    m_pRadioBtnAccount=new RadioBtn(this,"账号登录");
+
+    m_pEditAccount=new EditCtrl(this,"账号编辑框");
+    m_pEditPassword=new EditCtrl(this,"密码编辑框");
+
+    //设置ui控件状态
+    m_pRadioBtnTourist->selected(true); //游客按钮选中
+    m_pRadioBtnAccount->selected(false); //账号按钮取消选中
+
+    m_pEditAccount->enable(false); //账号编辑框设置为禁用
+    m_pEditPassword->enable(false); //密码编辑框设置为禁用
+
+    m_pLogin->enable(true); //登录设置为启用
+    m_pLogout->enable(false); //退出设置为启用
+}
+//当某个ui控件发生变化时调用该成员函数来通知中介者
+void ConcertMediator::ctrlChanged(CtrlParent *p_CtrlParent){
+    if(p_CtrlParent==m_pRadioBtnTourist){ //游客登录单选按钮被选中
+        m_pRadioBtnAccount->selected(false);
+        m_pRadioBtnTourist->selected(true);
+
+        m_pEditAccount->enable(false);
+        m_pEditPassword->enable(false);
+
+        m_pLogin->enable(true);
+
+    }else if(p_CtrlParent==m_pRadioBtnAccount){ //账号登录单选按钮被选中
+        m_pRadioBtnAccount->selected(true);
+        m_pRadioBtnTourist->selected(false);
+
+        m_pEditAccount->enable(false);
+        m_pEditPassword->enable(false);
+
+        if(m_pEditAccount->isContentEmpty() || m_pEditAccount->isContentEmpty()){
+            //账号 编辑框 或者 密码编辑框 有一个为空，则不允许登录
+            m_pLogin->enable(false);
+        }else{
+            //登录按钮启用
+            m_pLogin->enable(true);
+        }
+    }
+    if(p_CtrlParent==m_pEditAccount || p_CtrlParent==m_pEditPassword){ //账号 或 密码 编辑框内容发生改变
+        if(m_pEditAccount->isContentEmpty() || m_pEditAccount->isContentEmpty()){
+            //账号 编辑框 或者 密码编辑框 有一个为空，则不允许登录
+            m_pLogin->enable(false);
+        }else{
+            //登录按钮启用
+            m_pLogin->enable(true);
+        }
+    }
+}
+
+
+
+
+
--- MediatorPattern/MediatorPattern.h
+++ MediatorPattern/MediatorPattern.h
@@ -5,4 +5,106 @@
 #ifndef DESIGNPATTERNS_MEDIATORPATTERN_H
 #define DESIGNPATTERNS_MEDIATORPATTERN_H
 
+#include<iostream>
+using namespace std;
+
+//类前向声明
+class CtrlParent;
+
+//ui界面中介者接口
+class Mediator{
+public:
+    virtual ~Mediator(){}
+    virtual void createCtrl()=0; //创建所有需要用到的ui控件
+    virtual void ctrlChanged(CtrlParent* p_ctrl)=0; //某个ui空间发生变化时调用真接着对象的该函数来通知中介者
+};
+
+//ui控件的父类
+class CtrlParent{
+public:
+    //构造函数
+    CtrlParent(Mediator* p_media,string caption):m_pMediator(p_media),m_caption(caption){}
+    virtual ~CtrlParent(){}
+    //ui控件发生变化后调用
+    virtual void changed();
+    //设置ui控件启用或禁用
+    virtual void enable(bool sign)=0;
+protected:
+    Mediator* m_pMediator; //指向中介者对象
+    string m_caption; //ui控件名称
+};
+
+//普通按钮相关类
+class Button:public CtrlParent{
+public:
+    Button(Mediator* p_mediator,string caption): CtrlParent(p_mediator,caption){}
+    //设置按钮的启用或禁用
+    virtual void enable(bool sign);
+};
+
+//单选按钮类
+class RadioBtn:public CtrlParent{
+public:
+    RadioBtn(Mediator* p_mediator,string caption): CtrlParent(p_mediator,caption){}
+    //设置单选按钮的启用或禁用
+    virtual void enable(bool sign);
+    //设置单选按钮被选中或者被取消选中，被选中单选按钮中有个黑色实心圆点
+    void selected(bool sign);
+};
+
+//编辑框类
+class EditCtrl:public CtrlParent{
+public:
+    EditCtrl(Mediator* p_mediator,string caption): CtrlParent(p_mediator,caption){}
+    //设置编辑框的启用或禁用
+    virtual void enable(bool sign);
+    //判断编辑框中内容是否为空
+    bool isContentEmpty();
+    void setMContent(string content);
+    string getContent();
+private:
+    string m_content="";
+};
+
+//具体中介类
+class ConcertMediator:public Mediator{
+public:
+    //析构函数
+    ~ConcertMediator();
+    //创建ui控件
+    virtual void createCtrl();
+    //当某个ui控件发生变化时调用该成员函数来通知中介者
+    virtual void ctrlChanged(CtrlParent *p_CtrlParent);
+
+public:
+    Button * m_pLogin=nullptr; //登录按钮
+    Button * m_pLogout=nullptr; //退出按钮
+
+    RadioBtn * m_pRadioBtnTourist=nullptr; //游客登录单选框
+    RadioBtn * m_pRadioBtnAccount=nullptr; //账号登录单选框
+
+    EditCtrl * m_pEditAccount=nullptr; //账号编辑框
+    EditCtrl * m_pEditPassword=nullptr; //密码编辑框
+
+};
+
+
+
+
 #endif //DESIGNPATTERNS_MEDIATORPATTERN_H
+
+/*
+
+//使用案例
+void test01(){
+    //创建具体中介者
+    Mediator* p_media=new ConcertMediator();
+    //创建ui界面及控件
+    p_media->createCtrl();
+    cout<<"|---------------------------------------初始化完毕---------------------------------------|"<<endl;
+    cout<<"----------------------------------------当<账号登录>单选按钮被按下----------------------------------------"<<endl;
+    //模拟 账号登录 单选按钮被按下时，则去通知 中介者，有中介者实现 具体的逻辑处理
+    ((ConcertMediator *)p_media)->m_pRadioBtnAccount->changed();
+}
+
+ */
